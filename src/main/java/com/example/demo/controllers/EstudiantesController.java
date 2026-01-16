d6acb9f733178d6c5e88a105ac2fd67add75d4e4
lubswer <lubswer@hotmail.com>
2026-01-15 20:30:54 -0500

    Ejemplo de enrutamiento

--- src/main/java/com/example/demo/controllers/EstudiantesController.java
+++ src/main/java/com/example/demo/controllers/EstudiantesController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import com.example.demo.domain.Estudiante;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.domain.Estudiante;
 import java.util.ArrayList;
@@ -12,6 +13,17 @@ public class EstudiantesController {
     @GetMapping("/estudiantes")
     public ArrayList<Estudiante> obtenerRegistros(){
         return EstudianteRep.datosEstudiantes();
+    }
+    @GetMapping("/estudiantes/{name}")
+    public Estudiante obtenerEstudiante(@PathVariable String name){
+        ArrayList<Estudiante> estudiantes = EstudianteRep.datosEstudiantes();
+
+        for(Estudiante es : estudiantes){
+            if(es.getName().equalsIgnoreCase(name)){
+                return es;
+            }
+        }
+        return null;
     }
 
 
